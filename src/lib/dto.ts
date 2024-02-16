afb403478f295cee4fc0103612fc76caebf54e4f
ryzhovalex <thed4rkof@gmail.com>
2024-02-16 10:33:59 +0300

    actualize dto

--- src/lib/dto.ts
+++ src/lib/dto.ts
@@ -1,81 +1,12 @@
-import { UnsupportedError } from "./errors";
-
-/**
- * Host DTO is the outermost JSON body object sent from backend services.
- */
-export interface HostDTO<TDTO = any>  // `any` type for legacy support
+export interface Dto
 {
-    type: HostDTOType;
-    value: TDTO;
 }
 
-export enum HostDTOType
+export interface UnitDto extends Dto
 {
-    OK = "ok",
-    ERROR = "error"
+    sid: string;
 }
 
-export interface UnitDTO
+export interface Fdto extends Dto
 {
-    id: string;
-    code: string | null;
-}
-
-export interface ContainerDTO
-{
-    units: UnitDTO[];
-}
-
-export abstract class DTOUtils
-{
-  public static convertMany<T>(
-    hostdto: HostDTO<ContainerDTO>,
-    convertionFunc: (unit: UnitDTO) => T
-  ): T[];
-  public static convertMany<T>(
-    units: UnitDTO[],
-    convertionFunc: (unit: UnitDTO) => T
-  ): T[];
-  /**
-   * Converts items array into typescript objects using a convertion function.
-   *
-   * @param units dto units array to convert
-   * @param convertionFunc convertion function to use for each item in the
-   *                       array
-   * @returns array of typescript converted objects
-   */
-  public static convertMany<T>(
-    data: HostDTO<ContainerDTO> | UnitDTO[],
-    convertionFunc: (unit: UnitDTO) => T
-  ): T[]
-  {
-    if (this.isHostDTO(data))
-    {
-      return this.convertMany<T>(data.value.units, convertionFunc);
-    }
-
-    if (Array.isArray(data) && data.length > 0 && this.isUnitDTO(data[0]))
-    {
-      const result: T[] = [];
-
-      for (const unit of data)
-      {
-        result.push(convertionFunc(unit));
-      }
-
-      return result;
-    }
-
-    throw new UnsupportedError("convertMany data " + JSON.stringify(data));
-  }
-
-  public static isHostDTO(obj: object): obj is HostDTO
-  {
-    return Object.hasOwn(obj, "type") && Object.hasOwn(obj, "value");
-  }
-
-  public static isUnitDTO(obj: object): obj is UnitDTO
-  {
-    return Object.hasOwn(obj, "id");
-  }
 }
