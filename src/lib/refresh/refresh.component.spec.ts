aa5acbefc31dbc3b13b0d831788dc0768e2f1030
ryzhovalex <thed4rkof@gmail.com>
2024-03-13 10:23:12 +0300

    lint

--- src/lib/refresh/refresh.component.spec.ts
+++ src/lib/refresh/refresh.component.spec.ts
@@ -1,23 +1,26 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { RefreshComponent } from './refresh.component';
+import { RefreshComponent } from "./refresh.component";
 
-describe('RefreshComponent', () => {
+describe("RefreshComponent", () => 
+{
   let component: RefreshComponent;
   let fixture: ComponentFixture<RefreshComponent>;
 
-  beforeEach(async () => {
+  beforeEach(async () => 
+  {
     await TestBed.configureTestingModule({
       declarations: [ RefreshComponent ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(RefreshComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => 
+  {
     expect(component).toBeTruthy();
   });
 });
